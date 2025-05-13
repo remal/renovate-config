ad19a0116e7e01f9db3863e676a688956d5219ff
Semyon Levin <levin.semen@gmail.com>
2025-05-13 23:37:28 +0400

    Tue 05/13/2025 23:37:28.20

--- .github/workflows/build-process-log.js
+++ .github/workflows/build-process-log.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const logFile = process.argv[2]
 const log = '\n' + fs.readFileSync(logFile, 'utf8') + '\n'
 
-let newConfig = log.replaceFirst(/\n\s*WARN: Config migration necessary(\n[ \t]+)"oldConfig": \{[\s\S]+?\1\},\1"newConfig": (\{[\s\S]+?\1\})/, '$2')
+let newConfig = log.replace(/\n\s*WARN: Config migration necessary(\n[ \t]+)"oldConfig": \{[\s\S]+?\1\},\1"newConfig": (\{[\s\S]+?\1\})/, '$2')
 if (newConfig === log) {
     process.exit(1)
 }
 
 newConfig = JSON.stringify(JSON.parse(newConfig), null, 2)
-console.log(`Writing new config:\n  ${newConfig.replaceAll(/\n/, '\n  ')}`)
+console.log(`Writing new config:\n  ${newConfig.replace(/\n/g, '\n  ')}`)
 
 fs.writeFileSync(logFile, newConfig + '\n', encoding)
 
