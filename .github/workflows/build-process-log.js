34538373891457ff04dc38d07d8a1eee6ee21497
Semyon Levin <levin.semen@gmail.com>
2025-05-13 23:55:35 +0400

    Tue 05/13/2025 23:55:34.74

--- .github/workflows/build-process-log.js
+++ .github/workflows/build-process-log.js
@@ -5,12 +5,18 @@ const logFile = process.argv[3]
 
 const log = '\n' + fs.readFileSync(logFile, 'utf8') + '\n'
 
-let newConfig = log.replace(/[\s\S]*\n\s*WARN: Config migration necessary(\n[ \t]+)"oldConfig": \{[\s\S]+?\1\},\1"newConfig": (\{[\s\S]+?\1\})[\s\S]*/, '$2')
-if (newConfig === log) {
+let newConfig = log.replace(/[\s\S]*\n\s*WARN: Config migration necessary(\n[ \t]+)"oldConfig": \{\1[\s\S]+?\1\},\1"newConfig": \{\1([\s\S]+?)\1\}[\s\S]*/, '$2')
+if (newConfig === log || !newConfig.trim().length) {
     process.exit(1)
 }
 
-newConfig = JSON.stringify(JSON.parse(newConfig), null, 2)
+const newConfigPrefix = newConfig.replace(/^([ \t]+)[\s\S]*/, '$1')
+newConfig = newConfig.split('\n')
+    .map(line => line.substring(newConfigPrefix.length))
+    .join('\n')
+
+//newConfig = JSON.stringify(JSON.parse(newConfig), null, 2)
+console.log(`newConfig:\n${newConfig}`)
 
 console.log(`Migrating ${configFile}`)
 fs.writeFileSync(configFile, newConfig + '\n', 'utf8')
